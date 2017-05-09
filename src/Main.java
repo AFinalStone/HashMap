4f9c309149eae9f0c1a5571a6aa872ac53283497
AFinalStone <602392033@qq.com>
2017-05-09 14:26:59 +0800

    添加一个结合List和HashMap实现的例子
    Signed-off-by: AFinalStone <602392033@qq.com>

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,4 @@
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class Main {
 
@@ -52,5 +49,41 @@ public class Main {
         }
     }
 
+
+    /**
+     * 在不创建学生类的情况下，从键盘输入n个学生信息(学号，姓名，年龄），
+     * 输入完成后，打印出各个学生信息
+     * @author ccna_zhang
+     */
+    private static void test(){
+
+            //定义保存学生信息的List，元素类型为HashMap
+            List<HashMap<String, Object>> list = new ArrayList<HashMap<String, Object>>();
+            Scanner input = new Scanner(System.in);
+
+            System.out.println("请输入学生的信息,y表示继续，n表示退出");
+            while("y".equals(input.next())) {
+                HashMap<String, Object> map = new HashMap<String, Object>();
+                System.out.println("请输入学号");
+                map.put("studentno", input.next());
+                System.out.println("请输入姓名");
+                map.put("name", input.next());
+                System.out.println("请输入年龄");
+                map.put("age", input.nextInt());
+                list.add(map);
+                System.out.println("请继续输入学生的信息,y表示继续，n表示退出");
+            }
+
+            System.out.println("输入的学生信息为:");
+            System.out.println("学生数量为:" + list.size());
+
+            Iterator<HashMap<String, Object>> it = list.iterator();
+            int i = 1;
+            while(it.hasNext()) {
+                HashMap<String, Object> stuMap = it.next();
+                System.out.print("第" + i + "个学生的信息为");
+                System.out.println("学号:" + stuMap.get("studentno") + " ,姓名:" + stuMap.get("name") + " ,年龄:" + stuMap.get("age"));
+        }
+    }
 
 }
